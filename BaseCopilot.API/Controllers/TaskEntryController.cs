4e51be3cd4f5d66e7f1b007f969d51a756476ec8
alfonso <alfonso@DHARMA>
2024-09-23 21:00:24 -0600

    Add project files.

---
+++ BaseCopilot.API/Controllers/TaskEntryController.cs
@@ -0,0 +1,27 @@
+﻿using BaseCopilot.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseCopilot.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TaskEntryController : ControllerBase
+    {
+        private static List<TaskEntry> _taskEntry = new List<TaskEntry>
+        {
+            new TaskEntry
+            {
+                Id = 1,
+                Name = "Test",
+                Description = "Test",
+                End = DateTime.Now.AddHours(1),
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<TaskEntry>> GetAllTaskEntries()
+        {
+            return Ok(_taskEntry);
+        }
+    }
+}
---
+++ BaseCopilot.Shared/Entities/TaskEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace BaseCopilot.Shared.Entities
+{
+    public class TaskEntry
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public DateTime Start {  get; set; } = DateTime.Now;
+        public DateTime? End { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
